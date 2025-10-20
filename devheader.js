0591a6ee0376379c3fc9f993ba10e2d8c607f8f3
leo-reche <ladreche@gmail.com>
2025-10-20 12:29:51 +0200

    Update devheader.js

--- devheader.js
+++ devheader.js
@@ -118,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function() {
   banner.setAttribute("aria-live", "polite");
   banner.innerHTML = `
     <span>
-      Pickaxe is experiencing a server errors today. We're already in contact with them, and apologize for the inconvenience.
+      Pickaxe is experiencing a server errors today. We're already in contact with them, and apologize for any inconveniences!
     </span>
     <button class="fc-banner__close" type="button" aria-label="Dismiss announcement">×</button>
   `;
