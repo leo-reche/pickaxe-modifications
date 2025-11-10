7f194e68caa78e6beb360516e1931a1ba8b4de26
leo-reche <ladreche@gmail.com>
2025-11-10 07:51:21 -0300

    Update devfooter.js

--- devfooter.js
+++ devfooter.js
@@ -92,13 +92,15 @@ function stopButtonUpdate(){
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const txtBoxArea = document.querySelector('#studio-root textarea.resize-none').closest("div.relative.flex");
-  console.log(txtBoxArea)
-  const observer = new MutationObserver(stopButtonUpdate); //observe whole document for any changes
-      observer.observe(txtBoxArea, {
-      childList: true,
-      subtree: true
-  });
+  setTimeout(() => {
+    const txtBoxArea = document.querySelector('#studio-root textarea.resize-none').closest("div.relative.flex");
+    console.log(txtBoxArea)
+    const observer = new MutationObserver(stopButtonUpdate); //observe whole document for any changes
+        observer.observe(txtBoxArea, {
+        childList: true,
+        subtree: true
+    });
+  }, 1000); 
 });
 
 
